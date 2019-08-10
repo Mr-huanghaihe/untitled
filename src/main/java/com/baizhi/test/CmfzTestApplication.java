65a852dedc632a6d27dcc3edfc2003cff5dbfe1d
huanghh <1040097399@qq.com>
2019-08-10 12:20:45 +0800

    first create!

---
+++ src/main/java/com/baizhi/test/CmfzTestApplication.java
@@ -0,0 +1,13 @@
+package com.baizhi.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmfzTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmfzTestApplication.class, args);
+    }
+
+}
